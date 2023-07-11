9f8021c04454260b3e03105ec7cc11309178c4b1
hyder18 <Shammijb123@gmail.com>
2023-07-11 15:18:32 +0530

    roboshop-infra

--- var.tf
+++ var.tf
@@ -1,53 +1,48 @@
-variable "instances" {
+variable "instance" {
   default = {
     frontend = {
-    name = "frontend"
-    type = "t3.micro"
-  }
+      name = "frontend"
+      type = "t3.micro"
+    }
     mongodb = {
       name = "mongodb"
       type = "t3.micro"
     }
-   catalogue = {
-     name = "catalogue"
-     type = "t3.micro"
-    }
-    mysql = {
-      name = "mysql"
+    catalogue = {
+      name = "catalogue"
       type = "t3.micro"
-      password = "RoboShop@1"
     }
-    cart = {
-      name = "cart"
+    redis = {
+      name = "redis"
       type = "t3.micro"
     }
     user = {
       name = "user"
       type = "t3.micro"
     }
+    cart = {
+      name = "cart"
+      type = "t3.micro"
+    }
+    mysql = {
+      name = "mysql"
+      type = "t3.micro"
+      password = "RoboShop@1"
+    }
     shipping = {
       name = "shipping"
       type = "t3.micro"
       password = "RoboShop@1"
-    }
-    payment = {
-      name = "payment"
-      type = "t3.micro"
-      password = "roboshop123"
     }
     rabbitmq = {
       name = "rabbitmq"
       type = "t3.micro"
       password = "roboshop123"
     }
-    dispatch = {
-      name = "dispatch"
+    payment = {
+      name = "payment"
       type = "t3.micro"
       password = "roboshop123"
-    }
-    redis = {
-      name = "redis"
-      type = "t3.micro"
     }
   }
 }
