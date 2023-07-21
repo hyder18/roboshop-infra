61685d485a6997dba4f691b404a39de1027d9cc0
hyder18 <Shammijb123@gmail.com>
2023-07-21 12:12:20 +0530

    roboshop-ansible

--- ec2/main.tf
+++ ec2/main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "ansible-devops-practice"
+  name_regex  = "devops-practice with ansible"
   owners      = [data.aws_caller_identity.current.account_id]
 
 }
